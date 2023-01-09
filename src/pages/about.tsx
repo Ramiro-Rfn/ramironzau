a2a632b3d6addd38220cb440cba4f61e2992b17d
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2023-01-09 13:34:12 +0100

    making about page bee responsive

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -54,25 +54,25 @@ export default function About({ aboutMe, educations, workExperiences }: AboutPro
 
         <Box w='100%'>
             <Flex maxW={1190} align='center' justify='space-between' w='100%'  margin='0 auto 4rem' >
-                <Box maxW={1190} w='100%'   margin='6rem auto 0' id="about">
-                    <Flex w='100%'>
+                <Box maxW={1190} w='100%'   margin={['3rem 0 0','6rem auto 0']} px={['1rem', '2rem', '2rem', 0]} id="about">
+                    <Flex w='100%' flexDir={['column', 'column', 'column', 'row']}>
                         <Flex maxW={617} flexDir='column' >
                             <Text fontSize='2.5rem' color='gray.50' mb='4' >Sobre Me</Text>
-                            <Text fontSize='1.5rem' color='gray.100' fontWeight='400'>
+                            <Text fontSize={['1rem','1.5rem']} color='gray.100' fontWeight='400'>
                                 {aboutMe.aboutme}
                             </Text>
                         </Flex>
 
-                        <Flex flex='1' flexDir='column' mt='4rem' ml='4rem'>
-                            <Box mb='8'>
-                                <Text fontSize='2.5rem' color='gray.50' mb='4'>Experiência</Text>
+                        <Flex maxW={617} flex='1' flexDir='column' mt='4rem' ml={['0', 0, 0, '4rem']}>
+                            <Box mb={['8']}>
+                                <Text fontSize={['2rem','2.5rem']} color='gray.50' mb={['2','4']}>Experiência</Text>
                                 
                                 <Flex>
                                     {workExperiences?.map((workExperience)=> {
                                         return(
                                             <Flex key={workExperience.id} w='100%' flexDir='column' borderBottom='1px solid gray' py='2'>
                                                 <Flex align='center' justify='space-between'>
-                                                    <Text fontSize='1.25rem' color='gray.50' mb='2'>{workExperience.role}</Text>
+                                                    <Text fontSize='1.25rem' color='gray.50' mb='2' noOfLines={1}>{workExperience.role}</Text>
                                                     
                                                     <Text 
                                                     as='span' p='2' 
@@ -85,9 +85,9 @@ export default function About({ aboutMe, educations, workExperiences }: AboutPro
                                                     </Text>
                                                 </Flex>
             
-                                                <Flex align='center' justify='space-between'>
-                                                    <Text color='gray.100'>{workExperience.organization}</Text>
-                                                    <Text color='gray.100' textTransform='capitalize'>
+                                                <Flex align={['start','center']} justify='space-between' flexDir={['column', 'row']}>
+                                                    <Text color='gray.100' noOfLines={1}>{workExperience.organization}</Text>
+                                                    <Text color='gray.100' noOfLines={1} textTransform='capitalize'>
                                                         {`${workExperience.start} - ${workExperience.end}`}
                                                     </Text>
                                                 </Flex>
@@ -98,7 +98,7 @@ export default function About({ aboutMe, educations, workExperiences }: AboutPro
                             </Box>
 
                             <Box>
-                                <Text fontSize='2.5rem' color='gray.50' mb='4'>Formação</Text>
+                                <Text fontSize={['2rem', '2.5rem']} color='gray.50' mb={['2','4']}>Formação</Text>
                                 
                                 <Flex>
                                     <Flex w='100%' flexDir='column' py='2'>
@@ -109,7 +109,7 @@ export default function About({ aboutMe, educations, workExperiences }: AboutPro
                                                         <Text fontSize='1.25rem' color='gray.50' mb='2'>{education.course}</Text>
                                                     </Flex>
                 
-                                                    <Flex align='center' justify='space-between'>
+                                                    <Flex align={['start','center']} justify='space-between' flexDir={['column', 'row']}>
                                                         <Text color='gray.100'>{education.school}</Text>
                                                         <Text color='gray.100' textTransform='capitalize'>
                                                             {`${education.start} - ${education.end}`}
